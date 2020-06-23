65372387dc2028e991392709fe845715d706051b
Tim Carman <tpcarman@gmail.com>
2020-06-23 14:07:31 +1000

    Update README and default style script

--- AsBuiltReport.Veeam.VBR.Style.ps1
+++ AsBuiltReport.Veeam.VBR.Style.ps1
@@ -1,23 +1,23 @@
-# VMware Default Document Style
+# Veeam Default Document Style
 
 # Configure document options
 DocumentOption -EnableSectionNumbering -PageSize A4 -DefaultFont 'Arial' -MarginLeftAndRight 71 -MarginTopAndBottom 71 -Orientation $Orientation
 
 # Configure Heading and Font Styles
-Style -Name 'Title' -Size 24 -Color '485969' -Align Center
-Style -Name 'Title 2' -Size 18 -Color '006A91' -Align Center
-Style -Name 'Title 3' -Size 12 -Color '006A91' -Align Left
-Style -Name 'Heading 1' -Size 16 -Color '006A91' 
-Style -Name 'Heading 2' -Size 14 -Color '006A91' 
-Style -Name 'Heading 3' -Size 12 -Color '006A91' 
-Style -Name 'Heading 4' -Size 11 -Color '006A91' 
-Style -Name 'Heading 5' -Size 10 -Color '006A91'
+Style -Name 'Title' -Size 24 -Color '54B948' -Align Center
+Style -Name 'Title 2' -Size 18 -Color '565656' -Align Center
+Style -Name 'Title 3' -Size 12 -Color '565656' -Align Left
+Style -Name 'Heading 1' -Size 16 -Color '54B948' 
+Style -Name 'Heading 2' -Size 14 -Color '54B948' 
+Style -Name 'Heading 3' -Size 12 -Color '54B948' 
+Style -Name 'Heading 4' -Size 11 -Color '54B948' 
+Style -Name 'Heading 5' -Size 10 -Color '54B948'
 Style -Name 'Normal' -Size 10 -Color '565656' -Default
 Style -Name 'Caption' -Size 10 -Color '565656' -Italic -Align Center
 Style -Name 'Header' -Size 10 -Color '565656' -Align Center
 Style -Name 'Footer' -Size 10 -Color '565656' -Align Center
-Style -Name 'TOC' -Size 16 -Color '006A91' 
-Style -Name 'TableDefaultHeading' -Size 10 -Color 'FAFAFA' -BackgroundColor '485969'
+Style -Name 'TOC' -Size 16 -Color '54B948' 
+Style -Name 'TableDefaultHeading' -Size 10 -Color 'FAFAFA' -BackgroundColor '54B948'
 Style -Name 'TableDefaultRow' -Size 10 -Color '565656'
 Style -Name 'Critical' -Size 10 -Color '565656' -BackgroundColor 'F5DBD9'
 Style -Name 'Warning' -Size 10 -Color '565656' -BackgroundColor 'FEF3B5'
@@ -29,7 +29,7 @@ $TableDefaultProperties = @{
     Id = 'TableDefault'
     HeaderStyle = 'TableDefaultHeading'
     RowStyle = 'TableDefaultRow'
-    BorderColor = '485969'
+    BorderColor = '54B948'
     Align = 'Left'
     CaptionStyle = 'Caption'
     CaptionLocation = "Below" 
@@ -43,7 +43,7 @@ $TableDefaultProperties = @{
 TableStyle @TableDefaultProperties -Default
 TableStyle -Id 'Borderless' -HeaderStyle Normal -RowStyle Normal -BorderWidth 0
 
-# VMware Cover Page Layout
+# Veeam Cover Page Layout
 # Header & Footer
 Header -Default {
     Paragraph -Style Header "$($ReportConfig.Report.Name) - $($ReportConfig.Report.Version)"
