07a7d05471039e9b6c8f10f1d2df7e6a469672a0
Tim Carman <tpcarman@gmail.com>
2021-08-04 15:48:42 +1000

    Update module framework

--- AsBuiltReport.Veeam.VBR.Style.ps1
+++ AsBuiltReport.Veeam.VBR.Style.ps1
@@ -1,22 +1,22 @@
-# Veeam Default Document Style
+# Veeam VBR Default Document Style
 
 # Configure document options
 DocumentOption -EnableSectionNumbering -PageSize A4 -DefaultFont 'Arial' -MarginLeftAndRight 71 -MarginTopAndBottom 71 -Orientation $Orientation
 
 # Configure Heading and Font Styles
 Style -Name 'Title' -Size 24 -Color '54B948' -Align Center
-Style -Name 'Title 2' -Size 18 -Color '565656' -Align Center
-Style -Name 'Title 3' -Size 12 -Color '565656' -Align Left
-Style -Name 'Heading 1' -Size 16 -Color '54B948' 
-Style -Name 'Heading 2' -Size 14 -Color '54B948' 
-Style -Name 'Heading 3' -Size 12 -Color '54B948' 
-Style -Name 'Heading 4' -Size 11 -Color '54B948' 
+Style -Name 'Title 2' -Size 18 -Color '00447C' -Align Center
+Style -Name 'Title 3' -Size 12 -Color '00447C' -Align Left
+Style -Name 'Heading 1' -Size 16 -Color '54B948'
+Style -Name 'Heading 2' -Size 14 -Color '54B948'
+Style -Name 'Heading 3' -Size 12 -Color '54B948'
+Style -Name 'Heading 4' -Size 11 -Color '54B948'
 Style -Name 'Heading 5' -Size 10 -Color '54B948'
 Style -Name 'Normal' -Size 10 -Color '565656' -Default
 Style -Name 'Caption' -Size 10 -Color '565656' -Italic -Align Center
 Style -Name 'Header' -Size 10 -Color '565656' -Align Center
 Style -Name 'Footer' -Size 10 -Color '565656' -Align Center
-Style -Name 'TOC' -Size 16 -Color '54B948' 
+Style -Name 'TOC' -Size 16 -Color '54B948'
 Style -Name 'TableDefaultHeading' -Size 10 -Color 'FAFAFA' -BackgroundColor '54B948'
 Style -Name 'TableDefaultRow' -Size 10 -Color '565656'
 Style -Name 'Critical' -Size 10 -Color '565656' -BackgroundColor 'F5DBD9'
@@ -32,7 +32,7 @@ $TableDefaultProperties = @{
     BorderColor = '54B948'
     Align = 'Left'
     CaptionStyle = 'Caption'
-    CaptionLocation = "Below" 
+    CaptionLocation = 'Below'
     BorderWidth = 0.25
     PaddingTop = 1
     PaddingBottom = 1.5
@@ -43,23 +43,36 @@ $TableDefaultProperties = @{
 TableStyle @TableDefaultProperties -Default
 TableStyle -Id 'Borderless' -HeaderStyle Normal -RowStyle Normal -BorderWidth 0
 
-# Veeam Cover Page Layout
+# Veeam VBR Cover Page Layout
 # Header & Footer
-Header -Default {
-    Paragraph -Style Header "$($ReportConfig.Report.Name) - $($ReportConfig.Report.Version)"
-}
+if ($ReportConfig.Report.ShowHeaderFooter) {
+    Header -Default {
+        Paragraph -Style Header "$($ReportConfig.Report.Name) - v$($ReportConfig.Report.Version)"
+    }
 
-Footer -Default {
-    Paragraph -Style Footer 'Page <!# PageNumber #!>'
+    Footer -Default {
+        Paragraph -Style Footer 'Page <!# PageNumber #!>'
+    }
 }
 
 # Set position of report titles and information based on page orientation
+if (!($ReportConfig.Report.ShowCoverPageImage)) {
+    $LineCount = 5
+}
 if ($Orientation -eq 'Portrait') {
     BlankLine -Count 11
-    $LineCount = 30
+    $LineCount = 32 + $LineCount
 } else {
     BlankLine -Count 7
-    $LineCount = 20
+    $LineCount = 15 + $LineCount
+}
+
+# Veeam Logo Image
+if ($ReportConfig.Report.ShowCoverPageImage) {
+	# Always check the vendor's branding guidelines to ensure the use of their company logo is allowed.
+	# Convert a vendor's logo image to Base64 using https://base64.guru/converter/encode/image/jpg.
+	# Specify Base64 code using the `Base64` parameter below. Size image accordingly using the `Percent` parameter. Align image to center.
+    # Image -Text 'Veeam Logo' -Align 'Center' -Percent 5 -Base64 ""
 }
 
 # Add Report Name
@@ -67,6 +80,7 @@ Paragraph -Style Title $ReportConfig.Report.Name
 
 if ($AsBuiltConfig.Company.FullName) {
     # Add Company Name if specified
+    BlankLine -Count 2
     Paragraph -Style Title2 $AsBuiltConfig.Company.FullName
     BlankLine -Count $LineCount
 } else {
@@ -79,6 +93,8 @@ Table -Name 'Cover Page' -List -Style Borderless -Width 0 -Hashtable ([Ordered] 
     })
 PageBreak
 
-# Add Table of Contents
-TOC -Name 'Table of Contents'
-PageBreak
+if ($ReportConfig.Report.ShowTableOfContents) {
+    # Add Table of Contents
+    TOC -Name 'Table of Contents'
+    PageBreak
+}
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,13 @@
-# VMware Veeam Backup & Replication As Built Report Changelog
+# :arrows_counterclockwise: Veeam VBR As Built Report Changelog
 
-## [0.0.0] - Unreleased
+## [0.1.0] - YYYY-MM-DD
+
 ### Added
 
 ### Changed
 
 ### Fixed
+
+### Removed
+
+
