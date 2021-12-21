62de5d884c5f41c94d0b609395ef5d26698680b5
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-12-21 11:03:09 -0400

    Added a function to set the initial server connection

---
+++ Src/Private/Get-AbrVbrServerConnection.ps1
@@ -0,0 +1,61 @@
+function Get-AbrVbrServerConnection {
+    <#
+    .SYNOPSIS
+    Used by As Built Report to establish conection to Veeam B&R Server.
+    .DESCRIPTION
+    .NOTES
+        Version:        0.1.0
+        Author:         Jonathan Colon
+        Twitter:        @jcolonfzenpr
+        Github:         rebelinux
+    .EXAMPLE
+    .LINK
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+        Write-PscriboMessage "Establishing the initial connection to the Backup Server: $($System)."
+    }
+
+    process {
+        Write-PScriboMessage "Looking for veeam existing server connection."
+        $OpenConnection = (Get-VBRServerSession).Server
+        if($OpenConnection -eq $System) {
+            Write-PScriboMessage "Existing veeam server connection found"
+        }
+        elseif ($null -eq $OpenConnection) {
+            Write-PScriboMessage "No existing veeam server connection found"
+            try {
+                Write-PScriboMessage "Connecting to $($System) with privided credentials"
+                Connect-VBRServer -Server $System -Credential $Credential
+            }
+            catch {
+                Throw "Failed to connect to Veeam BR Host '$System' with user '$env:USERNAME'"
+            }
+        }
+        else {
+            Write-PScriboMessage "Actual veem server connection not equal to $($System). Disconecting connection."
+            Disconnect-VBRServer
+            try {
+                Write-PScriboMessage "Tring to open a new connection to $($System)"
+                Connect-VBRServer -Server $System -Credential $Credential
+            }
+            catch {
+                Throw "Failed to connect to Veeam BR Host '$System' with user '$env:USERNAME'"
+            }
+        }
+        Write-PScriboMessage "Validating connection to $($System)"
+        $NewConnection = (Get-VBRServerSession).Server
+        if ($null -eq $NewConnection) {
+            Throw "Failed to connect to Veeam BR Host '$System' with user '$env:USERNAME'"
+        }
+        elseif ($NewConnection) {
+            Write-PScriboMessage "Successfully connected to $($System) VBR Server."
+        }
+    }
+    end {}
+
+}
