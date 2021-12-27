3f1b6067221fc72093b60a53c746876cc563e42b
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-12-27 00:29:53 -0400

    Added function to retreive Veeam Virtual Labs

--- Src/Private/Get-AbrVbrSureBackup.ps1
+++ Src/Private/Get-AbrVbrSureBackup.ps1
@@ -26,7 +26,7 @@ function Get-AbrVbrSureBackup {
             Paragraph "The following section provides a summary of the Veeam SureBackup."
             BlankLine
             try {
-                Section -Style Heading3 'SureBackup Application Groups' {
+                Section -Style Heading4 'Application Groups' {
                     Paragraph "The following section provides a summary of the Veeam SureBackup Application Groups."
                     BlankLine
                     $OutObj = @()
@@ -47,7 +47,7 @@ function Get-AbrVbrSureBackup {
                     }
 
                     $TableParams = @{
-                        Name = "SureBackup Application Group - $(((Get-VBRServerSession).Server).ToString().ToUpper().Split(".")[0])"
+                        Name = "Application Group - $(((Get-VBRServerSession).Server).ToString().ToUpper().Split(".")[0])"
                         List = $false
                         ColumnWidths = 30, 20, 50
                     }
@@ -63,10 +63,10 @@ function Get-AbrVbrSureBackup {
             if ($InfoLevel.Infrastructure.SureBackup -ge 2) {
                 try {
                     $SureBackupAGs = Get-VBRApplicationGroup
-                    Section -Style Heading4 "$($VMSetting.Name) VM Settings" {
-                        Paragraph "The following section provides a detailed information of the VM Application Group Settings"
-                        BlankLine
-                        foreach ($SureBackupAG in $SureBackupAGs) {
+                    foreach ($SureBackupAG in $SureBackupAGs) {
+                        Section -Style Heading5 "$($SureBackupAG.Name) VM Settings" {
+                            Paragraph "The following section provides a detailed information of the VM Application Group Settings"
+                            BlankLine
                             try {
                                 foreach ($VMSetting in $SureBackupAG.VM) {
                                     Section -Style Heading5 "$($VMSetting.Name) VM Settings" {
@@ -76,9 +76,9 @@ function Get-AbrVbrSureBackup {
                                         Write-PscriboMessage "Discovered $($VMSetting.Name) Application Group VM Setting."
                                         $inObj = [ordered] @{
                                             'VM Name' = $VMSetting.Name
-                                            'Credentials' = $VMSetting.Credentials
-                                            'Role' = $VMSetting.Role -join ", "
-                                            'Test Script' = $VMSetting.TestScript.PredefinedApplication -join ", "
+                                            'Credentials' = ConvertTo-EmptyToFiller $VMSetting.Credentials
+                                            'Role' = ConvertTo-EmptyToFiller ($VMSetting.Role -join ", ")
+                                            'Test Script' = ConvertTo-EmptyToFiller ($VMSetting.TestScript.PredefinedApplication -join ", ")
                                             'Startup Options' = SWitch ($VMSetting.StartupOptions) {
                                                 "" {"-"; break}
                                                 $Null {"-"; break}
@@ -110,7 +110,43 @@ function Get-AbrVbrSureBackup {
                     Write-PscriboMessage $_.Exception.Message
                 }
             }
+            try {
+                Section -Style Heading4 'Virtual Labs' {
+                    Paragraph "The following section provides a summary of the Veeam SureBackup Virtual Lab."
+                    BlankLine
+                    $OutObj = @()
+                    try {
+                        $SureBackupVLs = Get-VBRVirtualLab
+                        foreach ($SureBackupVL in $SureBackupVLs) {
+                            Write-PscriboMessage "Discovered $($SureBackupVL.Name) Virtual Lab."
+                            $inObj = [ordered] @{
+                                'Name' = $SureBackupVL.Name
+                                'Platform' = $SureBackupVL.Platform
+                                'Physical Host' = $SureBackupVL.Server.Name
+                            }
+                            $OutObj += [pscustomobject]$inobj
+                        }
+                    }
+                    catch {
+                        Write-PscriboMessage $_.Exception.Message
+                    }
+
+                    $TableParams = @{
+                        Name = "Virtual Lab - $(((Get-VBRServerSession).Server).ToString().ToUpper().Split(".")[0])"
+                        List = $false
+                        ColumnWidths = 30, 20, 50
+                    }
+                    if ($Report.ShowTableCaptions) {
+                        $TableParams['Caption'] = "- $($TableParams.Name)"
+                    }
+                    $OutObj | Table @TableParams
+                }
+            }
+            catch {
+                Write-PscriboMessage $_.Exception.Message
+            }
         }
+
     }
     end {}
 
