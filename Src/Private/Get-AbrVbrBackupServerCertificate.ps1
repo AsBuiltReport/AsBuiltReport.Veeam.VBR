4f057e194f709c871fb757884ad3337fac03c8ef
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-12-29 20:33:32 -0400

    Added function to returns TLS certificates configured on Veeam Backup & Replication

---
+++ Src/Private/Get-AbrVbrBackupServerCertificate.ps1
@@ -0,0 +1,69 @@
+
+function Get-AbrVbrBackupServerCertificate {
+    <#
+    .SYNOPSIS
+    Used by As Built Report to returns TLS certificates configured on Veeam Backup & Replication.
+
+
+    .DESCRIPTION
+    .NOTES
+        Version:        0.1.0
+        Author:         Jonathan Colon
+        Twitter:        @jcolonfzenpr
+        Github:         rebelinux
+    .EXAMPLE
+    .LINK
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+
+    begin {
+        Write-PscriboMessage "Discovering Veeam VBR TLS certificates information from $System."
+    }
+
+    process {
+        Section -Style Heading4 'Backup Server TLS Certificate' {
+            Paragraph "The following section provides information of the configured TLS certificates on Veeam Backup & Replication."
+            BlankLine
+            $OutObj = @()
+            if ((Get-VBRServerSession).Server) {
+                try {
+                    $TLSSettings = Get-VBRBackupServerCertificate
+                    foreach ($EmailSetting in $TLSSettings) {
+                        $inObj = [ordered] @{
+                            'Friendly Name' = $TLSSettings.FriendlyName
+                            'Subject Name' = $TLSSettings.SubjectName
+                            'Issuer Name' = $TLSSettings.IssuerName
+                            'Expiration Date' = $TLSSettings.NotAfter.ToShortDateString()
+                            'Issued Date' = $TLSSettings.NotBefore.ToShortDateString()
+                            'Thumbprint' = $TLSSettings.Thumbprint
+                            'SerialNumber' = $TLSSettings.SerialNumber
+                        }
+                        $OutObj += [pscustomobject]$inobj
+                    }
+                }
+                catch {
+                    Write-PscriboMessage -IsWarning $_.Exception.Message
+                }
+
+                if ($HealthCheck.Infrastructure.Settings) {
+                    $OutObj | Where-Object { $_.'Enabled' -like 'No'} | Set-Style -Style Warning -Property 'Enabled'
+                }
+
+                $TableParams = @{
+                    Name = "TLS Certificate information - $(((Get-VBRServerSession).Server).ToString().ToUpper().Split(".")[0])"
+                    List = $true
+                    ColumnWidths = 40, 60
+                }
+                if ($Report.ShowTableCaptions) {
+                    $TableParams['Caption'] = "- $($TableParams.Name)"
+                }
+                $OutObj | Table @TableParams
+            }
+        }
+    }
+    end {}
+
+}
