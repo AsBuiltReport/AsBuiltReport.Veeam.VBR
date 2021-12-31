69eb160eef595e83a6db2a76bcbbf565f13ec04c
Jonathan Colon <jcolonf@zenprsolutions.com>
2021-12-30 21:18:52 -0400

    Added function to convert bytes automatically to GB or TB based on size

--- Src/Private/SharedUtilsFunctions.ps1
+++ Src/Private/SharedUtilsFunctions.ps1
@@ -101,3 +101,42 @@ function ConvertTo-VIobject {
         return $OBJECT
     }
 } # end
+function ConvertTo-FileSizeString {
+    <#
+    .SYNOPSIS
+    Used by As Built Report to convert bytes automatically to GB or TB based on size.
+    .DESCRIPTION
+    .NOTES
+        Version:        0.4.0
+        Author:         LEE DAILEY
+    .EXAMPLE
+    .LINK
+    #>
+    [CmdletBinding()]
+    [OutputType([String])]
+    Param
+        (
+        [Parameter (
+            Position = 0,
+            Mandatory)]
+            [int64]
+            $Size
+        )
+
+    switch ($Size) {
+        {$_ -gt 1TB}
+            {[string]::Format("{0:0} TB", $Size / 1TB); break}
+        {$_ -gt 1GB}
+            {[string]::Format("{0:0} GB", $Size / 1GB); break}
+        {$_ -gt 1MB}
+            {[string]::Format("{0:0} MB", $Size / 1MB); break}
+        {$_ -gt 1KB}
+            {[string]::Format("{0:0} KB", $Size / 1KB); break}
+        {$_ -gt 0}
+            {[string]::Format("{0} B", $Size); break}
+        {$_ -eq 0}
+            {"0 KB"; break}
+        default
+            {"0 KB"}
+        }
+    } # end >> function Format-FileSize
