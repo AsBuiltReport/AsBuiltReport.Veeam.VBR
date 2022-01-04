790b3d9f1808546908ff72ffc98546a82e7d3f58
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-01-04 00:31:27 -0400

    Added Tape Infrastructure Support

--- Src/Public/Invoke-AsBuiltReport.Veeam.VBR.ps1
+++ Src/Public/Invoke-AsBuiltReport.Veeam.VBR.ps1
@@ -38,50 +38,64 @@ function Invoke-AsBuiltReport.Veeam.VBR {
         Section -Style Heading1 'Implementation Report' {
             Paragraph "The following section provides a summary of the implemented components on the Veeam Backup & Replication Infrastructure"
             BlankLine
-            Section -Style Heading2 'Backup Infrastructure Summary' {
-                Get-AbrVbrInfrastructureSummary
-                Get-AbrVbrBackupServerInfo
-                Write-PScriboMessage "Infrastructure Licenses InfoLevel set at $($InfoLevel.Infrastructure.Licenses)."
-                if ($InfoLevel.Infrastructure.Licenses -ge 1) {
-                    Get-AbrVbrInstalledLicense
-                }
-                Write-PScriboMessage "Infrastructure Licenses InfoLevel set at $($InfoLevel.Infrastructure.Licenses)."
-                if ($InfoLevel.Infrastructure.Settings -ge 1) {
-                    Section -Style Heading3 'General Options' {
-                        Paragraph "The following section details the Veeam Veaam B&R general setting. General settings are applied to all jobs, backup infrastructure components and other objects managed by the backup server."
-                        BlankLine
-                        Get-AbrVbrEmailNotificationSetting
-                        Get-AbrVbrIOControlSetting
-                        Get-AbrVbrBackupServerCertificate
-                        Get-AbrVbrNetworkTrafficRule
+            Write-PScriboMessage "Backup Infrastructure InfoLevel set at $($InfoLevel.Infrastructure.Section)."
+            if ($InfoLevel.Infrastructure.Section -ge 1) {
+                Section -Style Heading2 'Backup Infrastructure Summary' {
+                    Get-AbrVbrInfrastructureSummary
+                    Get-AbrVbrBackupServerInfo
+                    Write-PScriboMessage "Infrastructure Licenses InfoLevel set at $($InfoLevel.Infrastructure.Licenses)."
+                    if ($InfoLevel.Infrastructure.Licenses -ge 1) {
+                        Get-AbrVbrInstalledLicense
+                    }
+                    Write-PScriboMessage "Infrastructure Settings InfoLevel set at $($InfoLevel.Infrastructure.Settings)."
+                    if ($InfoLevel.Infrastructure.Settings -ge 1) {
+                        Section -Style Heading3 'General Options' {
+                            Paragraph "The following section details the Veeam Veaam B&R general setting. General settings are applied to all jobs, backup infrastructure components and other objects managed by the backup server."
+                            BlankLine
+                            Get-AbrVbrEmailNotificationSetting
+                            Get-AbrVbrIOControlSetting
+                            Get-AbrVbrBackupServerCertificate
+                            Get-AbrVbrNetworkTrafficRule
+                        }
+                    }
+                    Get-AbrVbrUserRoleAssignment
+                    Get-AbrVbrCredential
+                    Get-AbrVbrLocation
+                    Get-AbrVbrManagedServer
+                    Write-PScriboMessage "Infrastructure Backup Proxy InfoLevel set at $($InfoLevel.Infrastructure.Proxy)."
+                    if ($InfoLevel.Infrastructure.Proxy -ge 1) {
+                        Get-AbrVbrBackupProxy
+                    }
+                    Write-PScriboMessage "Infrastructure WAN Accelerator InfoLevel set at $($InfoLevel.Infrastructure.WANAccel)."
+                    if ($InfoLevel.Infrastructure.WANAccel -ge 1) {
+                        Get-AbrVbrWANAccelerator
+                    }
+                    Write-PScriboMessage "Infrastructure Backup Repository InfoLevel set at $($InfoLevel.Infrastructure.BR)."
+                    if ($InfoLevel.Infrastructure.BR -ge 1) {
+                        Get-AbrVbrBackupRepository
+                        Get-AbrVbrObjectRepository
+                    }
+                    Write-PScriboMessage "Infrastructure ScaleOut Backup Repository InfoLevel set at $($InfoLevel.Infrastructure.SOBR)."
+                    if ($InfoLevel.Infrastructure.SOBR -ge 1) {
+                        Get-AbrVbrScaleOutRepository
+                    }
+                    Write-PScriboMessage "Infrastructure SureBackup InfoLevel set at $($InfoLevel.Infrastructure.SureBackup)."
+                    if ($InfoLevel.Infrastructure.SureBackup -ge 1) {
+                        Get-AbrVbrSureBackup
                     }
                 }
-                Get-AbrVbrUserRoleAssignment
-                Get-AbrVbrCredential
-                Get-AbrVbrLocation
-                Get-AbrVbrManagedServer
-                Write-PScriboMessage "Infrastructure Backup Proxy InfoLevel set at $($InfoLevel.Infrastructure.Proxy)."
-                if ($InfoLevel.Infrastructure.Proxy -ge 1) {
-                    Get-AbrVbrBackupProxy
-                }
-                Write-PScriboMessage "Infrastructure WAN Accelerator InfoLevel set at $($InfoLevel.Infrastructure.WANAccel)."
-                if ($InfoLevel.Infrastructure.WANAccel -ge 1) {
-                    Get-AbrVbrWANAccelerator
-                }
-                Write-PScriboMessage "Infrastructure Backup Repository InfoLevel set at $($InfoLevel.Infrastructure.BR)."
-                if ($InfoLevel.Infrastructure.BR -ge 1) {
-                    Get-AbrVbrBackupRepository
-                    Get-AbrVbrObjectRepository
-                }
-                Write-PScriboMessage "Infrastructure ScaleOut Backup Repository InfoLevel set at $($InfoLevel.Infrastructure.SOBR)."
-                if ($InfoLevel.Infrastructure.SOBR -ge 1) {
-                    Get-AbrVbrScaleOutRepository
-                }
-                Write-PScriboMessage "Infrastructure SureBackup InfoLevel set at $($InfoLevel.Infrastructure.SureBackup)."
-                if ($InfoLevel.Infrastructure.SureBackup -ge 1) {
-                    Get-AbrVbrSureBackup
+            }
+            Write-PScriboMessage "Tape Infrastructure InfoLevel set at $($InfoLevel.Tape.Section)."
+            if ($InfoLevel.Tape.Section -ge 1) {
+                Section -Style Heading2 'Tape Infrastructure Summary' {
+                    Write-PScriboMessage "Tape Server InfoLevel set at $($InfoLevel.Tape.Server)."
+                    if ($InfoLevel.Tape.Server -ge 1) {
+                        Get-AbrVbrTapeServer
+                        Get-AbrVbrTapeLibrary
+                    }
                 }
             }
+
         }
         #Disconnect-VBRServer
 	}
