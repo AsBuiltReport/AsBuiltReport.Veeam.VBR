f4137f0f40592b0e76e24402654ff78a14b700d7
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-01-07 23:55:06 -0400

    Improved the Tape Section with more content

--- Src/Private/Get-AbrVbrTapeLibrary.ps1
+++ Src/Private/Get-AbrVbrTapeLibrary.ps1
@@ -32,77 +32,125 @@ function Get-AbrVbrTapeLibrary {
                         $TapeObjs = Get-VBRTapeLibrary
                         foreach ($TapeObj in $TapeObjs) {
                             try {
-                                Write-PscriboMessage "Discovered $($TapeObj.Name) Type Library."
-                                $TapeServer = (Get-VBRTapeServer | Where-Object {$_.Id -eq $TapeObj.TapeServerId}).Name
-                                $inObj = [ordered] @{
-                                    'Library Name' = $TapeObj.Name
-                                    'Library Model' = $TapeObj.Model
-                                    'Library Type' = $TapeObj.Type
-                                    'Number of Slots' = $TapeObj.Slots
-                                    'Connected to' = $TapeServer
-                                    'Enabled' = ConvertTo-TextYN $TapeObj.Enabled
-                                    'Status' = Switch ($TapeObj.State) {
-                                        'Online' {'Available'}
-                                        'Offline' {'Unavailable'}
-                                        default {$TapeObj.State}
+                                Section -Style Heading3 "$($TapeObj.Name) Tape Library" {
+                                    Write-PscriboMessage "Discovered $($TapeObj.Name) Type Library."
+                                    $TapeServer = (Get-VBRTapeServer | Where-Object {$_.Id -eq $TapeObj.TapeServerId}).Name
+                                    $inObj = [ordered] @{
+                                        'Library Name' = $TapeObj.Name
+                                        'Library Model' = $TapeObj.Model
+                                        'Library Type' = $TapeObj.Type
+                                        'Number of Slots' = $TapeObj.Slots
+                                        'Connected to' = $TapeServer
+                                        'Enabled' = ConvertTo-TextYN $TapeObj.Enabled
+                                        'Status' = Switch ($TapeObj.State) {
+                                            'Online' {'Available'}
+                                            'Offline' {'Unavailable'}
+                                            default {$TapeObj.State}
+                                        }
                                     }
-                                }
-                                $OutObj = [pscustomobject]$inobj
 
-                                if ($HealthCheck.Tape.Status) {
-                                    $OutObj | Where-Object { $_.'Status' -eq 'Unavailable'} | Set-Style -Style Warning -Property 'Status'
-                                }
+                                    $OutObj += [pscustomobject]$inobj
 
-                                $TableParams = @{
-                                    Name = "Tape Library - $($TapeObj.Name)"
-                                    List = $true
-                                    ColumnWidths = 40, 60
-                                }
+                                    if ($HealthCheck.Tape.Status) {
+                                        $OutObj | Where-Object { $_.'Status' -eq 'Unavailable'} | Set-Style -Style Warning -Property 'Status'
+                                    }
 
-                                if ($Report.ShowTableCaptions) {
-                                    $TableParams['Caption'] = "- $($TableParams.Name)"
-                                }
-                                $OutObj | Table @TableParams
-                                $DriveObjs = Get-VBRTapeDrive -Library $TapeObj.Id
-                                if ($DriveObjs) {
-                                    Write-PscriboMessage "Collecting $($TapeObj.Name) Tape Drives"
-                                    Section -Style Heading4 "$($TapeObj.Name) Tape Drives" {
-                                        Paragraph "The following section provides information on $($TapeObj.Name) Tape Drives."
-                                        BlankLine
-                                        $OutObj = @()
-                                        if ((Get-VBRServerSession).Server) {
-                                            try {
-                                                foreach ($DriveObj in $DriveObjs) {
-                                                    Write-PscriboMessage "Discovered $($DriveObj.Name) Type Drive."
-                                                    $inObj = [ordered] @{
-                                                        'Name' = $DriveObj.Name
-                                                        'Model' = $DriveObj.Model
-                                                        'Serial Number' = $DriveObj.SerialNumber
-                                                        'Medium' = $DriveObj.Medium
-                                                        'Enabled' = ConvertTo-TextYN $DriveObj.Enabled
-                                                        'Is Locked' = ConvertTo-TextYN $DriveObj.IsLocked
-                                                        'State' = $DriveObj.State
+                                    $TableParams = @{
+                                        Name = "Tape Library - $($TapeObj.Name)"
+                                        List = $true
+                                        ColumnWidths = 40, 60
+                                    }
+
+                                    if ($Report.ShowTableCaptions) {
+                                        $TableParams['Caption'] = "- $($TableParams.Name)"
+                                    }
+                                    $OutObj | Table @TableParams
+                                    #---------------------------------------------------------------------------------------------#
+                                    #                                  Tape Drives Section                                        #
+                                    #---------------------------------------------------------------------------------------------#
+                                    $DriveObjs = Get-VBRTapeDrive -Library $TapeObj.Id
+                                    if ($DriveObjs) {
+                                        Write-PscriboMessage "Collecting $($TapeObj.Name) Tape Drives"
+                                        Section -Style Heading4 "Tape Drives" {
+                                            $OutObj = @()
+                                            if ((Get-VBRServerSession).Server) {
+                                                try {
+                                                    foreach ($DriveObj in $DriveObjs) {
+                                                        Write-PscriboMessage "Discovered $($DriveObj.Name) Type Drive."
+                                                        $inObj = [ordered] @{
+                                                            'Name' = $DriveObj.Name
+                                                            'Model' = $DriveObj.Model
+                                                            'Serial Number' = $DriveObj.SerialNumber
+                                                            'Medium' = $DriveObj.Medium
+                                                            'Enabled' = ConvertTo-TextYN $DriveObj.Enabled
+                                                            'Is Locked' = ConvertTo-TextYN $DriveObj.IsLocked
+                                                            'State' = $DriveObj.State
+                                                        }
+                                                        $OutObj += [pscustomobject]$inobj
                                                     }
-                                                    $OutObj += [pscustomobject]$inobj
-                                                }
 
-                                                if ($HealthCheck.Tape.Status) {
-                                                    $OutObj | Where-Object { $_.'Status' -eq 'Unavailable'} | Set-Style -Style Warning -Property 'Status'
-                                                }
+                                                    if ($HealthCheck.Tape.Status) {
+                                                        $OutObj | Where-Object { $_.'Status' -eq 'Unavailable'} | Set-Style -Style Warning -Property 'Status'
+                                                    }
 
-                                                $TableParams = @{
-                                                    Name = "Tape Drives - $($TapeObj.Name)"
-                                                    List = $false
-                                                    ColumnWidths = 14, 18, 16, 16, 12, 12, 12
-                                                }
+                                                    $TableParams = @{
+                                                        Name = "Tape Drives - $($TapeObj.Name)"
+                                                        List = $false
+                                                        ColumnWidths = 14, 18, 16, 16, 12, 12, 12
+                                                    }
 
-                                                if ($Report.ShowTableCaptions) {
-                                                    $TableParams['Caption'] = "- $($TableParams.Name)"
+                                                    if ($Report.ShowTableCaptions) {
+                                                        $TableParams['Caption'] = "- $($TableParams.Name)"
+                                                    }
+                                                    $OutObj | Table @TableParams
                                                 }
-                                                $OutObj | Table @TableParams
+                                                catch {
+                                                    Write-PscriboMessage -IsWarning $_.Exception.Message
+                                                }
                                             }
-                                            catch {
-                                                Write-PscriboMessage -IsWarning $_.Exception.Message
+                                        }
+                                    }
+                                    #---------------------------------------------------------------------------------------------#
+                                    #                                  Tape Medium Section                                        #
+                                    #---------------------------------------------------------------------------------------------#
+                                    $MediumObjs = Get-VBRTapeMedium -Library $TapeObj.Id
+                                    if ($MediumObjs) {
+                                        Write-PscriboMessage "Collecting $($TapeObj.Name) Tape Medium"
+                                        Section -Style Heading4 "Tape Mediums" {
+                                            $OutObj = @()
+                                            if ((Get-VBRServerSession).Server) {
+                                                try {
+                                                    foreach ($MediumObj in $MediumObjs) {
+                                                        Write-PscriboMessage "Discovered $($MediumObj.Name) Type Medium."
+                                                        $inObj = [ordered] @{
+                                                            'Name' = $MediumObj.Name
+                                                            'Expiration Date' = Switch (($MediumObj.ExpirationDate).count) {
+                                                                0 {"-"}
+                                                                default {$MediumObj.ExpirationDate.ToShortDateString()}
+                                                            }
+                                                            'Total Space' = ConvertTo-FileSizeString $MediumObj.Capacity
+                                                            'Free Space' = ConvertTo-FileSizeString $MediumObj.Free
+                                                            'Locked' = ConvertTo-TextYN $MediumObj.IsLocked
+                                                            'Retired' = ConvertTo-TextYN $MediumObj.IsRetired
+                                                            'Worm' = ConvertTo-TextYN $MediumObj.IsWorm
+                                                        }
+                                                        $OutObj += [pscustomobject]$inobj
+                                                    }
+
+                                                    $TableParams = @{
+                                                        Name = "Tape Mediums - $($TapeObj.Name)"
+                                                        List = $false
+                                                        ColumnWidths = 30, 16, 12, 12, 10, 10, 10
+                                                    }
+
+                                                    if ($Report.ShowTableCaptions) {
+                                                        $TableParams['Caption'] = "- $($TableParams.Name)"
+                                                    }
+                                                    $OutObj | Table @TableParams
+                                                }
+                                                catch {
+                                                    Write-PscriboMessage -IsWarning $_.Exception.Message
+                                                }
                                             }
                                         }
                                     }
