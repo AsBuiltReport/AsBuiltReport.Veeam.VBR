948fb71212b4f69bcd1e70be300b344efb52d5d3
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-02-04 23:03:34 -0400

    Bump file version to 0.3.1

--- Src/Public/Invoke-AsBuiltReport.Veeam.VBR.ps1
+++ Src/Public/Invoke-AsBuiltReport.Veeam.VBR.ps1
@@ -5,7 +5,7 @@ function Invoke-AsBuiltReport.Veeam.VBR {
     .DESCRIPTION
         Documents the configuration of Veeam VBR in Word/HTML/Text formats using PScribo.
     .NOTES
-        Version:        0.3.0
+        Version:        0.3.1
         Author:         Jonathan Colon
         Twitter:        @jcolonfzenpr
         Github:         rebelinux
