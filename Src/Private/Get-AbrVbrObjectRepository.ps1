a4a0bcf48c8db58b34dd486d673868a40eb14e77
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-04-22 21:10:50 -0400

    Bumped file version

--- Src/Private/Get-AbrVbrObjectRepository.ps1
+++ Src/Private/Get-AbrVbrObjectRepository.ps1
@@ -6,7 +6,7 @@ function Get-AbrVbrObjectRepository {
     .DESCRIPTION
         Documents the configuration of Veeam VBR in Word/HTML/Text formats using PScribo.
     .NOTES
-        Version:        0.4.0
+        Version:        0.4.1
         Author:         Jonathan Colon
         Twitter:        @jcolonfzenpr
         Github:         rebelinux
