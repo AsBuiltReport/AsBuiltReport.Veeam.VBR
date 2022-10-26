810bf160084501d46ece02a6be2c54e03ee554b2
Jonathan Colon <jcolonf@zenprsolutions.com>
2022-10-25 21:12:12 -0400

    Initial work Tape Media Pool section do not  show all the configured Tape Library  #72

--- Src/Private/Get-AbrVbrTapeMediaPool.ps1
+++ Src/Private/Get-AbrVbrTapeMediaPool.ps1
@@ -72,6 +72,61 @@ function Get-AbrVbrTapeMediaPool {
                     catch {
                         Write-PscriboMessage -IsWarning $_.Exception.Message
                     }
+                    Section -Style Heading4 'Tape Media Pools Configuration' {
+                        foreach ($PoolObj in ($PoolObjs | Where-Object {$_.Type -eq 'Gfs' -or $_.Type -eq 'Custom'})) {
+                            Section -Style Heading5 $PoolObj.Name {
+                                Section -ExcludeFromTOC -Style NOTOCHeading6 'Tape Library' {
+                                    $OutObj = @()
+                                    foreach ($TapeLibrary in $PoolObj.GlobalOptions.LibraryId) {
+                                        try {
+                                            $TapeLibraryObj = Get-VBRTapeLibrary -Id $TapeLibrary.Guid
+                                            if ($PoolObj.Type -ne "Custom") {
+                                                $Capacity = ((Get-VBRTapeMedium -MediaPool $PoolObj.Id | Where-Object { $_.LibraryId -eq $TapeLibrary.Guid}).Capacity | Measure-Object -Sum).Sum
+                                                $FreeSpace = ((Get-VBRTapeMedium -MediaPool $PoolObj.Id | Where-Object { $_.LibraryId -eq $TapeLibrary.Guid}).Free | Measure-Object -Sum).Sum
+                                            }
+                                            else {
+                                                $Capacity = $PoolObj.Capacity
+                                                $FreeSpace = $PoolObj.FreeSpace
+                                            }
+                                            $TapeDrives = @()
+                                            foreach ($Drive in $TapeLibraryObj.Drives) {
+                                                $TapeDrives += "Drive $($Drive.Address + 1)"
+                                            }
+                                            Write-PscriboMessage "Discovered $($PoolObj.Name) Media Pool Configuration."
+                                            $inObj = [ordered] @{
+                                                'Name' = $TapeLibraryObj.Name
+                                                'Type' = $TapeLibraryObj.Type
+                                                'State' = $TapeLibraryObj.State
+                                                'Model' = $TapeLibraryObj.Model
+                                                'Drives' = $TapeDrives -join ', '
+                                                'Slots' = $TapeLibraryObj.Slots
+                                                'Description' = $TapeLibraryObj.Description
+                                                'Tape Count' = ((Get-VBRTapeMediaPool -Id $PoolObj.Id).Medium).count
+                                                'Total Space' = ConvertTo-FileSizeString $Capacity
+                                                'Free Space' = ConvertTo-FileSizeString $FreeSpace
+                                            }
+
+                                            $OutObj = [pscustomobject]$inobj
+
+                                            $TableParams = @{
+                                                Name = "Tape Library - $($PoolObj.Name)"
+                                                List = $true
+                                                ColumnWidths = 40, 60
+                                            }
+
+                                            if ($Report.ShowTableCaptions) {
+                                                $TableParams['Caption'] = "- $($TableParams.Name)"
+                                            }
+                                            $OutObj | Sort-Object -Property 'Name' | Table @TableParams
+                                        }
+                                        catch {
+                                            Write-PscriboMessage -IsWarning $_.Exception.Message
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
