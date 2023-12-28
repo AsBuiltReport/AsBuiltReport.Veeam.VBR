9cebf0f068706eb507a80a7054d2ff0b0f068431
Jonathan Colon <rebelinux@gmail.com>
2023-12-28 17:17:10 -0400

    Add support for Object Storage on Unstructured Data #118

--- Src/Private/Get-AbrVbrInventorySummary.ps1
+++ Src/Private/Get-AbrVbrInventorySummary.ps1
@@ -6,7 +6,7 @@ function Get-AbrVbrInventorySummary {
     .DESCRIPTION
         Documents the configuration of Veeam VBR in Word/HTML/Text formats using PScribo.
     .NOTES
-        Version:        0.7.1
+        Version:        0.8.3
         Author:         Jonathan Colon
         Twitter:        @jcolonfzenpr
         Github:         rebelinux
