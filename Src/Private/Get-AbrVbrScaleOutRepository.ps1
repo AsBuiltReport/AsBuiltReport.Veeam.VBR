27c964d9bbd41719beec4deeb85b40257cb54484
Jonathan Colon <rebelinux@gmail.com>
2024-01-16 12:01:01 -0400

    Migrate Windows Time Period code to separate function #133

--- Src/Private/Get-AbrVbrScaleOutRepository.ps1
+++ Src/Private/Get-AbrVbrScaleOutRepository.ps1
@@ -227,49 +227,7 @@ function Get-AbrVbrScaleOutRepository {
                                                             }
                                                             $OutObj = @()
                                                             try {
-                                                                $Days = 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
-                                                                $Hours24 = [ordered]@{
-                                                                    0 = 12
-                                                                    1 = 1
-                                                                    2 = 2
-                                                                    3 = 3
-                                                                    4 = 4
-                                                                    5 = 5
-                                                                    6 = 6
-                                                                    7 = 7
-                                                                    8 = 8
-                                                                    9 = 9
-                                                                    10 = 10
-                                                                    11 = 11
-                                                                    12 = 12
-                                                                    13 = 1
-                                                                    14 = 2
-                                                                    15 = 3
-                                                                    16 = 4
-                                                                    17 = 5
-                                                                    18 = 6
-                                                                    19 = 7
-                                                                    20 = 8
-                                                                    21 = 9
-                                                                    22 = 10
-                                                                    23 = 11
-                                                                }
-                                                                $ScheduleTimePeriod = $BackupRepo.OffloadWindowOptions -split '(.{48})' | Where-Object {$_}
-
-                                                                foreach ($OBJ in $Hours24.GetEnumerator()) {
-
-                                                                    $inObj = [ordered] @{
-                                                                        'H' = $OBJ.Value
-                                                                        'Sun' = $ScheduleTimePeriod[0].Split(',')[$OBJ.Key]
-                                                                        'Mon' = $ScheduleTimePeriod[1].Split(',')[$OBJ.Key]
-                                                                        'Tue' = $ScheduleTimePeriod[2].Split(',')[$OBJ.Key]
-                                                                        'Wed' = $ScheduleTimePeriod[3].Split(',')[$OBJ.Key]
-                                                                        'Thu' = $ScheduleTimePeriod[4].Split(',')[$OBJ.Key]
-                                                                        'Fri' = $ScheduleTimePeriod[5].Split(',')[$OBJ.Key]
-                                                                        'Sat' = $ScheduleTimePeriod[6].Split(',')[$OBJ.Key]
-                                                                    }
-                                                                    $OutObj += $inobj
-                                                                }
+                                                                $OutObj = Get-WindowsTimePeriod -InputTimePeriod $BackupRepo.OffloadWindowOptions
 
                                                                 $TableParams = @{
                                                                     Name = "Offload Window - $(($CapacityExtent.Repository).Name)"
--- Src/Private/SharedUtilsFunctions.ps1
+++ Src/Private/SharedUtilsFunctions.ps1
@@ -436,3 +436,73 @@ function Get-ColumnChart {
     return $Base64Image
 
 } # end
+
+function Get-WindowsTimePeriod {
+    <#
+    .SYNOPSIS
+    Used by As Built Report to generate time period table.
+    .DESCRIPTION
+    .NOTES
+        Version:        0.1.0
+        Author:         Jonathan Colon
+    .EXAMPLE
+    .LINK
+    #>
+    [CmdletBinding()]
+    Param
+        (
+        [Parameter (
+            Position = 0,
+            Mandatory)]
+            [System.Array]
+            $InputTimePeriod
+        )
+
+    $OutObj = @()
+    $Days = 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
+    $Hours24 = [ordered]@{
+        0 = 12
+        1 = 1
+        2 = 2
+        3 = 3
+        4 = 4
+        5 = 5
+        6 = 6
+        7 = 7
+        8 = 8
+        9 = 9
+        10 = 10
+        11 = 11
+        12 = 12
+        13 = 1
+        14 = 2
+        15 = 3
+        16 = 4
+        17 = 5
+        18 = 6
+        19 = 7
+        20 = 8
+        21 = 9
+        22 = 10
+        23 = 11
+    }
+    $ScheduleTimePeriod = $InputTimePeriod -split '(.{48})' | Where-Object {$_}
+
+    foreach ($OBJ in $Hours24.GetEnumerator()) {
+
+        $inObj = [ordered] @{
+            'H' = $OBJ.Value
+            'Sun' = $ScheduleTimePeriod[0].Split(',')[$OBJ.Key]
+            'Mon' = $ScheduleTimePeriod[1].Split(',')[$OBJ.Key]
+            'Tue' = $ScheduleTimePeriod[2].Split(',')[$OBJ.Key]
+            'Wed' = $ScheduleTimePeriod[3].Split(',')[$OBJ.Key]
+            'Thu' = $ScheduleTimePeriod[4].Split(',')[$OBJ.Key]
+            'Fri' = $ScheduleTimePeriod[5].Split(',')[$OBJ.Key]
+            'Sat' = $ScheduleTimePeriod[6].Split(',')[$OBJ.Key]
+        }
+        $OutObj += $inobj
+    }
+
+    return $OutObj
+
+} # end
