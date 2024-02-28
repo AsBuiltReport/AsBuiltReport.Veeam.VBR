c36467bcf27f79f742d90127645b720451cf2ef7
Jonathan Colon <rebelinux@gmail.com>
2024-02-28 12:42:14 -0400

    Fix chart temporary save location

--- Src/Private/SharedUtilsFunctions.ps1
+++ Src/Private/SharedUtilsFunctions.ps1
@@ -335,7 +335,9 @@ function Get-PieChart {
     }
     Add-ChartTitle @addChartTitleParams
 
-    $ChartImage = Export-Chart -Chart $exampleChart -Path (Get-Location).Path -Format "PNG" -PassThru
+    $TempPath = Resolve-Path ([System.IO.Path]::GetTempPath())
+
+    $ChartImage = Export-Chart -Chart $exampleChart -Path $TempPath.Path -Format "PNG" -PassThru
 
     $Base64Image = [convert]::ToBase64String((Get-Content $ChartImage -Encoding byte))
 
@@ -419,7 +421,9 @@ function Get-ColumnChart {
     }
     Add-ChartTitle @addChartTitleParams
 
-    $ChartImage = Export-Chart -Chart $exampleChart -Path (Get-Location).Path -Format "PNG" -PassThru
+    $TempPath = Resolve-Path ([System.IO.Path]::GetTempPath())
+
+    $ChartImage = Export-Chart -Chart $exampleChart -Path $TempPath.Path -Format "PNG" -PassThru
 
     if ($PassThru) {
         Write-Output -InputObject $chartFileItem
